17f342b59c046df37e4bad3b1dbaacc6c5b906f3
Anargyros Argyros <argyrosan@gmail.com>
2024-05-10 16:37:25 +0300

    checkpoints and some of fetch operations

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,18 @@
-use std::collections::HashMap;
 use std::fmt;
 
-use std::error::Error;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 //use std::io::Write;
 
-use std::net::TcpStream;
-use std::io::Write;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
-
-// define the schema 
-fn suggest_database_schema(user_schema: &HashMap<String, String>) -> String {
-    // Column definitions will be stored here
-    let mut column_definitions = Vec::new();
-
-    // Define your data type mapping
-    let data_type_map = HashMap::from([
-        ("string".to_string(), "TEXT".to_string()),
-        ("integer".to_string(), "INTEGER".to_string()),
-        ("float".to_string(), "REAL".to_string()),
-        ("boolean".to_string(), "BOOLEAN".to_string()),
-    ]);
-
-    // Iterate over the user schema
-    for (column, data_type) in user_schema {
-        let default_type = "TEXT".to_string(); // Create the default type
-        let db_data_type = data_type_map
-                .get(data_type)
-                .unwrap_or(&default_type); // Use the reference
-
-        column_definitions.push(format!("{} {}", column, db_data_type));
-    }
-
-    // Construct the CREATE TABLE statement
-    let table_name = "user_data"; // Customize as needed
-    format!(
-        "CREATE TABLE {} ({});",
-        table_name,
-        column_definitions.join(", ")
-    )
-}
-
-  
 //let's say a dataframe
 #[derive(Serialize, Deserialize, Debug)]
 struct Dataframe {
     // data and schema fields
     //execution graph
-    operations: Vec<OpNode>, 
+    operations: Vec<OpNode>,
+    checkpoint: usize
 }
 
 //we have these operations
@@ -58,17 +23,18 @@ enum OperationType {
     Where,
     Sum,
     Count,
+    Fetch,
     Empty, // it is for the initialization
 }
 
-// define the node 
+// define the node
 #[derive(Serialize, Deserialize, Debug)]
 struct OpNode {
     //id
-    lazy: bool, // if it is lazy t means it just creates a new node to the graph
-    function_name: OperationType,//the function name
-    args: Vec<String>,//arguments about the function
-    //to add more fields
+    id: usize, // if it is lazy t means it just creates a new node to the graph
+    function_name: OperationType, //the function name
+    args: Vec<String>, //arguments about the function
+               //to add more fields
 }
 
 // display the execution graph and the operation type of each node.
@@ -76,6 +42,7 @@ impl fmt::Display for OperationType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             OperationType::Select => write!(f, "Select"),
+            OperationType::Fetch => write!(f, "Fetch"),
             OperationType::Where => write!(f, "Where"),
             OperationType::Sum => write!(f, "Sum"),
             OperationType::Count => write!(f, "Count"),
@@ -85,78 +52,121 @@ impl fmt::Display for OperationType {
     }
 }
 
+static mut ID: usize = 0;
 impl Dataframe {
-
     fn new() -> Self {
-        Dataframe { 
+        Dataframe {
             operations: vec![OpNode {
-                lazy: false,
+                id: 0,
                 function_name: OperationType::Empty,
                 args: vec![],
             }],
+            checkpoint: 0,
         }
     }
 
     fn select(&mut self, arg: String) -> &mut Self {
-        
         let arguments: Vec<String> = arg.split(" ").map(|s| s.to_string()).collect();
-        
+        unsafe { ID = ID+1 };
+        let current_id = unsafe { ID };
         let select_node = OpNode {
-            lazy: true, 
+            id: current_id,
             function_name: OperationType::Select,
-            args: arguments, 
+            args: arguments,
         };
         self.operations.push(select_node);
-        self 
+        self
     }
 
     fn where_(&mut self, condition: String) -> &mut Self {
-
         let arguments: Vec<String> = condition.split(" ").map(|s| s.to_string()).collect();
-
+        unsafe { ID = ID+1 };
+        let current_id = unsafe { ID };
         let where_node = OpNode {
-            lazy: true,
+            id: current_id,
             function_name: OperationType::Where,
-            args: arguments, 
+            args: arguments,
         };
         self.operations.push(where_node);
-        self 
+        self
     }
 
     fn sum(&mut self, column: String) -> &mut Self {
+        unsafe { ID = ID+1 };
+        let current_id = unsafe { ID };
+        print!("Sum of collumn {}: ", column);
         let sum_node = OpNode {
-            lazy: false, 
+            id: current_id,
             function_name: OperationType::Sum,
             args: vec![column],
         };
         self.operations.push(sum_node);
         self.send_graph();
+        self.get_response();
+        println!("");
+        //clear graph, its already been executed at this point
+        self.operations = vec![];
+        //mark latest operation executed id
+        self.checkpoint = current_id;
         self
     }
 
-    fn count(&mut self, column: String) -> &mut Self {
+    fn count(&mut self) -> &mut Self {
+        unsafe { ID = ID+1 };
+        let current_id = unsafe { ID };
         let count_node = OpNode {
-            lazy: true,
+            id: current_id,
             function_name: OperationType::Count,
-            args: vec![column],
+            args: vec![],
         };
         self.operations.push(count_node);
         self.send_graph();
+        print!("Counted ");
+        self.get_response();
+        print!(" total rows\n");
+        
+        //clear graph, its already been executed at this point
+        self.operations = vec![];
+        //mark latest operation executed id
+        self.checkpoint = current_id;
         self
     }
 
     fn read(&mut self, filename: String) -> &mut Self {
+        unsafe { ID = ID+1 };
+        let current_id = unsafe { ID };
         let read_node = OpNode {
-            lazy: false, 
+            id: current_id,
             function_name: OperationType::Read,
             args: vec![filename],
         };
+        //lazy operation -> just append to graph
         self.operations.push(read_node);
         self
     }
 
+    fn fetch(&mut self) -> &mut Self {
+        unsafe { ID = ID+1 };
+        let current_id = unsafe { ID };
+        let fetch_node = OpNode {
+            id: current_id,
+            function_name: OperationType::Fetch,
+            args: vec![],
+        };
+        //add node to graph
+        self.operations.push(fetch_node);
+        //send graph for execution
+        self.send_graph();
+        //get response
+        self.get_response();
+        //clear graph, its already been executed at this point
+        self.operations = vec![];
+        //mark latest operation executed id
+        self.checkpoint = current_id;
+        self
+    }
 
-    fn send_graph(&self){
+    fn send_graph(&self) {
         //--graph in json file--
         let json = serde_json::to_string(self).expect("couldnt serialize graph");
         //--send the execution graph via a socket
@@ -166,30 +176,38 @@ impl Dataframe {
         stream.write(msg).unwrap();
     }
 
+    fn get_response(&self) {
+        let listener = TcpListener::bind("127.0.0.1:8001").unwrap();
+        for stream in listener.incoming() {
+            let mut stream = stream.unwrap();
+
+            let mut buffer = [0; 512];
+            let read_size = stream.read(&mut buffer).unwrap();
+            let s = std::str::from_utf8(&buffer[0..read_size])
+                .expect("Not a valid UTF-8 sequence")
+                .to_string();
+
+            print!("{}", s);
+            break;
+        }
+    }
 }
 
 
-////////////////////////////////////////////////////////////////////////
-/// 
-
-
-fn main(){
-
+fn main() {
     //dataframe
     let mut dataframe = Dataframe::new();
 
     //filename
-    let filename: String = "deniro.csv".to_string();
 
     //--graph in memory--
     //the graph is vector of operation nodes.
-    dataframe.read(filename.clone());
-    dataframe.select("column1 column3".to_string());
-    dataframe.where_("column2 > 10".to_string());           
-    dataframe.sum("column1".to_string()); 
-    dataframe.count("column1".to_string()); 
-
+    dataframe.read("deniro.csv".to_string());
+    dataframe.select("Year Title".to_string());
+    dataframe.where_("Score > 90".to_string());
+    dataframe.sum("Year".to_string());
+    dataframe.sum("Year".to_string());
+    dataframe.sum("Score".to_string());
+    // dataframe.fetch();
+    dataframe.count();
 }
-
-
-
